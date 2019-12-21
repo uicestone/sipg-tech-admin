f19c558b3eab08a59f0677fbdbb2bec2266850e1
Uice Lu <uicestone@gmail.com>
2019-12-21 22:37:06 +0800

    disable eslint for prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
     node: true
   },
 
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: [
+    "plugin:vue/essential"
+    //, "@vue/prettier"
+  ],
 
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
